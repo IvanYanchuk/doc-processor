7f2a088c907eee95546910dc87c01338c858ba02
IvanYanchuk <vania_yanchuk@mail.ru>
2024-04-12 16:53:58 +0200

    done

--- solution/README.md
+++ solution/README.md
@@ -12,3 +12,15 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `npx cdk deploy`  deploy this stack to your default AWS account/region
 * `npx cdk diff`    compare deployed stack with current state
 * `npx cdk synth`   emits the synthesized CloudFormation template
+
+## Activate user
+```
+aws cognito-idp admin-set-user-password --user-pool-id <user-pool-id> --username <username> --password <password> --permanent
+```
+<user-pool-id> can be found in cfn outputs or on the User pool details page.
+
+## Get auth token
+```
+aws cognito-idp initiate-auth --auth-flow USER_PASSWORD_AUTH --client-id <client_id> --auth-parameters USERNAME=<username>,PASSWORD=<password>
+```
+<client_id> can be found in the "App integration" tab in the User pool details page
--- solution/lib/file-api-stack.ts
+++ solution/lib/file-api-stack.ts
@@ -1,10 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import { EndpointType, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { AuthorizationType, CognitoUserPoolsAuthorizer, EndpointType, MethodOptions, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Bucket, IBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { join } from 'path';
+import { UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito';
 
 interface FileApiStackProps extends cdk.StackProps {
     docsBucket: IBucket
@@ -15,13 +16,6 @@ export class FileApiStack extends cdk.Stack {
     super(scope, id, props);
 
     const bucket = Bucket.fromBucketArn(this, 'ImportedBucket', props.docsBucket.bucketArn);
-
-    const api = new RestApi(this, 'FileApi', {
-        binaryMediaTypes: ["*/*"],
-        endpointConfiguration: {
-            types: [EndpointType.EDGE]
-        }
-    });
 
     // Create a Lambda function
     const fileProcessorLambda = new NodejsFunction(this, 'FileProcessorLambda', {
@@ -36,12 +30,56 @@ export class FileApiStack extends cdk.Stack {
     bucket.grantPut(fileProcessorLambda);
     bucket.grantPutAcl(fileProcessorLambda);
 
+    const userPool = new UserPool(this, 'FileApiUserPool', {
+        selfSignUpEnabled: true,
+        signInAliases: {
+            username: true
+        }
+    });
+
+    const userPoolClient = userPool.addClient('FileApiUserPoolClient', {
+        authFlows: {
+            adminUserPassword: true,
+            custom: true,
+            userPassword: true,
+            userSrp: true
+        }
+    });
+
+    const api = new RestApi(this, 'FileApi', {
+        binaryMediaTypes: ["*/*"],
+        endpointConfiguration: {
+            types: [EndpointType.EDGE]
+        }
+    });
+
+    const authorizer = new CognitoUserPoolsAuthorizer(this, "FileApiAuthorizer", {
+        cognitoUserPools: [ userPool ],
+        identitySource: 'method.request.header.Authorization'
+    });
+    authorizer._attachToApi(api);
+
+    const optionWithAuth: MethodOptions = {
+        authorizationType: AuthorizationType.COGNITO,
+        authorizer: {
+            authorizerId: authorizer.authorizerId
+        }
+    }
+
     // Plug the Lambda function into API Gateway, and enable CORS
     const uploadFileResource = api.root.addResource('uploadFile');
-    uploadFileResource.addMethod('POST', new cdk.aws_apigateway.LambdaIntegration(fileProcessorLambda));
+    uploadFileResource.addMethod('POST', new cdk.aws_apigateway.LambdaIntegration(fileProcessorLambda), optionWithAuth);
     uploadFileResource.addCorsPreflight({
       allowOrigins: ['*'],
       allowMethods: ['POST'],
+    });
+
+    new cdk.CfnOutput(this, 'FileApiUserPoolId', {
+        value: userPool.userPoolId
+    });
+
+    new cdk.CfnOutput(this, 'FileApiUserPoolClientId', {
+        value: userPoolClient.userPoolClientId
     });
 
     // const bucketItemResource = api.root.addResource("{item}");
